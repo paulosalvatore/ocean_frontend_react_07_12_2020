684cb937dd92790af22134f71f45d7013bd4dd44
Paulo Salvatore <salvatoregames@gmail.com>
2020-12-07 15:23:20 -0300

    Criação do tabuleiro e dos componentes Game, Board e Square

--- src/index.js
+++ src/index.js
@@ -1,8 +1,49 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function Square(props) {
+    return <button className="square">X</button>
+}
+
+function Board(props) {
+    return (
+        <div>
+            <div className="board-row">
+                <Square/>
+                <Square/>
+                <Square/>
+            </div>
+            <div className="board-row">
+                <Square/>
+                <Square/>
+                <Square/>
+            </div>
+            <div className="board-row">
+                <Square/>
+                <Square/>
+                <Square/>
+            </div>
+        </div>
+    )
+}
+
+function Game(props) {
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board/>
+            </div>
+
+            <div className="game-info">
+                Info
+            </div>
+        </div>
+    );
+}
+
 ReactDOM.render(
-    <div>Hello world!</div>,
+    // Elemento que será renderizado
+    <Game/>,
+    // Local em que esse elemento será renderizado
     document.getElementById('root')
 );
